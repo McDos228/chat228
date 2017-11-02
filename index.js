48bc41206cc46d8141319f1cbdd4d7da8682e72d
Stas <canyon2011@ukr.net>
2017-11-02 12:42:47 +0200

    v 0.0.1.1

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
+//var express = require('express');
+//var app = express();
+//var port = 3000;
+//
+//app.use(express.static(__dirname + '/public'));
+//
+//app.get('/', function(req, res){
+//	res.sendFile(__dirname + '/index.html');
+//});
+//
+//app.listen(port, function(){
+//	console.log('server start ' + port);
+//})
+
 var express = require('express');
 var app = express();
 var port = 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, function(){
 	console.log('server start ' + port);
 })
-
