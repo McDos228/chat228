fd84525ed7a6cd3a1d05cfddfa442d26e5e0825f
Stas <canyon2011@ukr.net>
2017-11-02 12:46:02 +0200

    v 0.0.1.1

--- index.js
+++ index.js
@@ -14,14 +14,18 @@
 
 var express = require('express');
 var app = express();
-var port = 3000;
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, function(){
-	console.log('server start ' + port);
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
