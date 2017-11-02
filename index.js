673d0c7e001db2543238a1a5d2a80363f809eb02
Stas <canyon2011@ukr.net>
2017-11-02 12:49:22 +0200

    test

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-//var express = require('express');
-//var app = express();
-//var port = 3000;
-//
-//app.use(express.static(__dirname + '/public'));
-//
-//app.get('/', function(req, res){
-//	res.sendFile(__dirname + '/index.html');
-//});
-//
-//app.listen(port, function(){
-//	console.log('server start ' + port);
-//})
-
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
