49ebf9cac14d5eed3e1bb9ee45eb2b3cc0f74652
Stas <canyon2011@ukr.net>
2017-11-09 14:53:48 +0200

    new server

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
-var app = express();
-var port = 3000;
+var app = require('express')();
 var http = require('http').Server(app);
-var io =require('socket.io')(http);
-var bodyParser = require('body-parser');
+var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 5000));
 
+var users = [];
+var connections = [];
+
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
@@ -40,7 +41,6 @@ io.on('connection', function(socket){
 	});
 });
 
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+http.listen(app.get('port'), function(){
+  console.log('server start on port', app.get('port'));
 });
